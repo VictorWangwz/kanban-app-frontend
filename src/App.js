742dbba9cc9467858c0a269423ab1b490870da17
VictorWangwz <wangzhen5161@outlook.com>
2019-01-17 10:49:23 -0800

    add design, components, use router-dom

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
+import Navbar from "./components/NavBar";
+import ProjectBoard from "./components/ProjectBoard";
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import AddProjectTask from './components/ProjectTask/AddProjectTask';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      <h1 className="alert alert-warning">
-        Welcome
-      </h1>
-      </div>
+      <Router>
+        <div className="App">
+
+          <Navbar />
+          <Route exact path="/" component={ProjectBoard}></Route>
+          <Route exact path="/addProjectTask" component={AddProjectTask}></Route>
+        </div>
+      </Router>
     );
   }
 }
