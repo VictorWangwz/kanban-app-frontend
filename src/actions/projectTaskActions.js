63ec4d2588d476282fff556ec2a006d329087ea7
VictorWangwz <wangzhen5161@outlook.com>
2019-01-18 12:43:58 -0800

    get from server and map to the var in render

--- src/actions/projectTaskActions.js
+++ src/actions/projectTaskActions.js
@@ -1,14 +1,14 @@
 import axios from "axios";
-import {GET_ERRORS }from "./types";
+import {GET_ERRORS, GET_PROJECT_TASKS }from "./types";
 
 export const addProjectTask = (project_task, history) => async dispatch =>{
     try{
         await axios.post("http://localhost:8080/api/board", project_task);
         history.push("/");// redirect function in history
-        // dispatch({
-        //     type: GET_ERRORS,
-        //     payload: {}
-        // })
+        dispatch({
+            type: GET_ERRORS,
+            payload: {}
+        })
     }catch(error){
         dispatch({
             type: GET_ERRORS,
@@ -16,4 +16,12 @@ export const addProjectTask = (project_task, history) => async dispatch =>{
         })
     }
     
+}
+
+export const getBackLog = () => async dispatch =>{
+    const response = await axios.get("http://localhost:8080/api/board/all")
+    dispatch({
+        type: GET_PROJECT_TASKS,
+        payload: response.data
+    })
 }
--- src/components/ProjectBoard.js
+++ src/components/ProjectBoard.js
@@ -1,9 +1,49 @@
-import React, { Component } from 'react'
-import {Link} from "react-router-dom"
+import React, { Component } from 'react';
+import {Link} from "react-router-dom";
 import ProjectTaskItem from './ProjectTask/ProjectTaskItem';
-class ProjectBoard extends Component {
+import {connect} from "react-redux";
+import PropTypes from "prop-types";
+import {getBackLog} from "../actions/projectTaskActions";
 
+class ProjectBoard extends Component {
+    //life cycle hook
+    componentDidMount(){
+        this.props.getBackLog();
+    };
+    
   render() {
+      const {project_tasks} = this.props.project_tasks;
+    let BoardContent;
+    let toDoItems = [];
+    let inProgressItems = [];
+    let doneItems = [];
+    
+    const BoardAlgorithm = project_tasks =>{
+        if (project_tasks.length <1){
+            return (
+                <div className="alert" role="alert">
+                    No project board
+                </div>
+            )
+        }else{
+            const tasks = project_tasks.map(project_task=>(
+                <ProjectTaskItem key={project_task.id} project_task={project_task}/>
+            ));
+            for(let i=0; i<tasks.length; i++){
+                if(tasks[i].props.project_task.status == "TO_DO"){
+                    toDoItems.push(tasks[i]);
+                }
+                if(tasks[i].props.project_task.status == "IN_PROGRESS"){
+                    inProgressItems.push(tasks[i]);
+                }
+                if(tasks[i].props.project_task.status == "DONE"){
+                    doneItems.push(tasks[i]);
+                }
+            }
+        }
+    }
+    BoardAlgorithm(project_tasks);
+
     return (
         <div className="container">
         <Link to="/addProjectTask" className="btn btn-primary mb-3">
@@ -25,10 +65,11 @@ class ProjectBoard extends Component {
 
                     {//<!-- SAMPLE PROJECT TASK STARTS HERE -->
                     }
-                   <ProjectTaskItem></ProjectTaskItem>
+                  
 
                     {//<!-- SAMPLE PROJECT TASK ENDS HERE -->
                     }
+                    {toDoItems}
                 </div>
                 <div className="col-md-4">
                     <div className="card text-center mb-2">
@@ -38,10 +79,10 @@ class ProjectBoard extends Component {
                     </div>
                     {//<!-- SAMPLE PROJECT TASK STARTS HERE -->
                     }
-                    <ProjectTaskItem></ProjectTaskItem>
 
                     {//<!-- SAMPLE PROJECT TASK ENDS HERE -->
                     }
+                    {inProgressItems}
 
                 </div>
                 <div className="col-md-4">
@@ -52,10 +93,10 @@ class ProjectBoard extends Component {
                     </div>
                     {//<!-- SAMPLE PROJECT TASK STARTS HERE -->
                     }
-                    <ProjectTaskItem></ProjectTaskItem>
 
                     {//<!-- SAMPLE PROJECT TASK ENDS HERE -->
                     }
+                    {doneItems}
                 </div>
             </div>
         </div>
@@ -67,4 +108,16 @@ class ProjectBoard extends Component {
     );
   }
 }
-export default ProjectBoard
+
+ProjectBoard.propTypes = {
+    getBackLog: PropTypes.func.isRequired,
+    project_tasks: PropTypes.object.isRequired
+
+};
+
+//state is from redux reducer
+const mapStateToProps = state => ({
+    project_tasks: state.project_task
+});
+
+export default connect(mapStateToProps, {getBackLog})(ProjectBoard);
